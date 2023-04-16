b2f2710fde53d1d9e20818fbf2accb9f8bb1c420
aps08 <anoopprsingh.social@gmail.com>
2023-04-16 17:39:06 +0530

    second api

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-var express = require('express')
-var app = express()
+var express = require("express");
+var app = express();
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.set("port", process.env.PORT || 5000);
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.get("/", function (request, response) {
+  response.send("Hello World!");
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
+app.get("/api2", function (request, response) {
+  response.send("<button>Go to aps08 profile</button>");
+});
+
+app.listen(app.get("port"), function () {
+  console.log("Node app is running at localhost:" + app.get("port"));
+});
